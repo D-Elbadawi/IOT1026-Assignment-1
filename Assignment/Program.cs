54158fbf39a4f126504684d863e5d1e1772c2172
D-Elbadawi <130947785+D-Elbadawi@users.noreply.github.com>
2023-05-17 19:36:24 -0400

    Update Program.cs
    The program has been worked well

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment
+﻿using System;
+// Assignment 1 After update
+namespace Assignment
 {
     public static class ArrayReplicator
     {
@@ -9,7 +11,9 @@
         /// <returns>A deep copy of the original array</returns>
         public static int[] ReplicateArray(int[] original)
         {
-            throw new NotImplementedException();
+            int[] copy = new int[original.Length];
+            Array.Copy(original, copy, original.Length);
+            return copy;
         }
 
         /// <summary>
@@ -19,7 +23,18 @@
         /// <returns>The user input as an integer</returns>
         public static int AskForNumber(string text)
         {
-            throw new NotImplementedException();
+            Console.Write(text);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.Write("Please enter a valid integer: ");
+                }
+            }
         }
 
         /// <summary>
@@ -30,14 +45,27 @@
         /// <param name="min">Smallest permissible value</param>
         /// <param name="max">Largest permissible value</param>
         /// <returns>The user input as an integer</returns>
-        public static int AskForNumberInRange(string text, int min, int max) => throw new NotImplementedException();
+        public static int AskForNumberInRange(string text, int min, int max)
+        {
+            Console.Write(text);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int number) && number >= min && number <= max)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.Write($"Please enter a valid integer between {min} and {max}: ");
+                }
+            }
+        }
     }
 
     static class Program
     {
         static void Main()
         {
-            
             const int Min = 0;
             const int Max = 10;
             const int PrintOffset = 4;
@@ -54,8 +82,9 @@
             int[] copy = ArrayReplicator.ReplicateArray(original);
             // Verify original and replicated array are the same
             for (int index = 0; index < size; ++index)
+            {
                 Console.WriteLine($"Original {original[index],-PrintOffset}  {copy[index],4} Copy");
-            
+            }
         }
     }
 }
